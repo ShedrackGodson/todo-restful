e90aec1594064ac2915236612da87f067bd7be0c
ShedrackGodson <hedrickgodson23@gmail.com>
2020-11-07 11:47:32 +0300

    serializing tasks objects to get taks json objects

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import api_overview
+from .views import api_overview, task_list
 
 
 urlpatterns = [
     path("", api_overview, name="api-overview"),
+    path("task-list/", task_list, name="task-list"),
 ]
--- api/views.py
+++ api/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .serializers import TaskSerializer
+from .models import Task
 
 # Create your views here.
 @api_view(['GET'])
@@ -15,3 +16,10 @@ def api_overview(request):
         "Delete": "/task-delete/<str:pk>/",
     }
     return Response(api_urls)
+
+
+@api_view(['GET'])
+def task_list(request):
+    tasks = Task.objects.all()[::-1] # Querying our objects
+    serializer = TaskSerializer(tasks, many=True) # Serializing our objects to be Json Objects
+    return Response(serializer.data,) # Returning the serialized data
