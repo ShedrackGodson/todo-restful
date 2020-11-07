487777dce893eb4267a0cba27209838ea1e4b131
ShedrackGodson <hedrickgodson23@gmail.com>
2020-11-07 12:03:54 +0300

    serializing single task to display details and handle exceptions when object is not available

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import api_overview, task_list
+from .views import (
+    api_overview, task_list, task_detail
+)
 
 
 urlpatterns = [
     path("", api_overview, name="api-overview"),
     path("task-list/", task_list, name="task-list"),
+    path("task-detail/<int:pk>/", task_detail, name="task-detail"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.decorators import api_view
+# from django.db.models import DoesNotExist
 from rest_framework.response import Response
 from .serializers import TaskSerializer
+from django.http import JsonResponse
+from django.shortcuts import render
 from .models import Task
 
 # Create your views here.
@@ -10,16 +11,26 @@ from .models import Task
 def api_overview(request):
     api_urls = {
         "List": "/task-list/",
-        "Detail View": "/task-details/<str:pk>/",
+        "Detail View": "/task-details/<int:pk>/",
         "Create": "/task-create/",
-        "Update": "/task-update/<str:pk>/",
-        "Delete": "/task-delete/<str:pk>/",
+        "Update": "/task-update/<int:pk>/",
+        "Delete": "/task-delete/<int:pk>/",
     }
     return Response(api_urls)
 
 
-@api_view(['GET'])
+@api_view(['GET']) # This allows only GET Method
 def task_list(request):
     tasks = Task.objects.all()[::-1] # Querying our objects
-    serializer = TaskSerializer(tasks, many=True) # Serializing our objects to be Json Objects
+    serializer = TaskSerializer(tasks, many=True) # Serializing our objects to get Json Objects
     return Response(serializer.data,) # Returning the serialized data
+
+
+@api_view(['GET'])
+def task_detail(request, pk):
+    try:
+        task = Task.objects.get(pk=pk) # Querying our object
+    except Task.DoesNotExist:
+        return Response("Object Not Available", status=404)
+    serializer = TaskSerializer(task, many=False) # Serializing our object to get Json Object
+    return Response(serializer.data)
