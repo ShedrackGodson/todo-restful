2d2f522a62a0736e3fa83465d5c85cfb34d2638a
ShedrackGodson <hedrickgodson23@gmail.com>
2020-11-07 22:10:23 +0300

    frontend app

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     api_overview, task_list, task_detail, task_create,
-    task_update,
+    task_update, task_delete
 )
 
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path("task-list/", task_list, name="task-list"),
     path("task-detail/<int:pk>/", task_detail, name="task-detail"),
     path("task-update/<int:pk>/", task_update, name="task-update"),
+    path("task-delete/<int:pk>/", task_delete, name="task-delete"),
 ]
--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import TaskSerializer
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Task
 
 # Create your views here.
@@ -23,7 +23,7 @@ def api_overview(request):
 def task_list(request):
     tasks = Task.objects.all()[::-1] # Querying our objects
     serializer = TaskSerializer(tasks, many=True) # Serializing our objects to get Json Objects
-    return Response(serializer.data,) # Returning the serialized data
+    return Response(serializer.data, status=200) # Returning the serialized data
 
 
 @api_view(['GET'])
@@ -33,7 +33,7 @@ def task_detail(request, pk):
     except Task.DoesNotExist:
         return Response("Object Not Available", status=404)
     serializer = TaskSerializer(task, many=False) # Serializing our object to get Json Object
-    return Response(serializer.data)
+    return Response(serializer.data,status=200)
 
 
 @api_view(['POST'])
@@ -41,15 +41,25 @@ def task_create(request):
     serializer = TaskSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-    return Response(serializer.data)
+    return Response(serializer.data, status=200)
 
 
 @api_view(['POST'])
 def task_update(request, pk):
     task = Task.objects.get(pk=pk)
     serializer = TaskSerializer(instance=task, data=request.data)
-    
+
     if serializer.is_valid():
         serializer.save()
-    return Response(serializer.data)
+    return Response(serializer.data, status=200)
+
+
+@api_view(['DELETE'])
+def task_delete(request, pk):
+    try:
+        task = Task.objects.get(pk=pk)
+    except Task.DoesNotExist:
+        return Response("Object Does not exists.", status=404)
+    task.delete()
+    return Response("Item successfully deleted.", status=200)
 
